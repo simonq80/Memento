4b6a5a7422c654b7604fdc6d1c60afbfd6b896ec
simonq80 <simonq80@gmail.com>
2015-10-09 14:55:57 +0100

    first

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+
+public class Main {
+
+	public class originator{
+		public String data;
+		public int rev_no;
+		
+		public memento create_mem(){
+			
+			return 
+		}
+		
+		
+	}
+	
+	public class memento{
+		public String data;
+		public int rev_no;
+		public memento(String dat, int rev){
+			data = dat;
+			rev_no = rev;
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
